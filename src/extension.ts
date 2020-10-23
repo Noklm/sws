ff4be3700afbe0b8f75a1ccbf4429ec091e91467
judland <julien.dland@gmail.com>
2020-10-23 16:23:35 +0200

    Add build commands with configurations

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const build = "sws.build";
+	const clean = "sws.clean";
+	const flash = "sws.flash";
+	const showConfig = "sws.showConfig";
+	
+	const showConfigHandler = (): void => {
+		const config = vscode.workspace.getConfiguration('sws');
+		vscode.window.showInformationMessage(
+			`Target: ${config.get('config.target')}`
+		);
+	};
+	const commandHandler = (msg: String): void => {
+		const config = vscode.workspace.getConfiguration('sws');
+		vscode.window.activeTerminal?.sendText(`make -s ${msg} TARGET=${config.get('config.target')}`);
+	};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sws" is now active!');
+	let dispoBuild = vscode.commands.registerCommand(build, commandHandler);
+	let dispoClean = vscode.commands.registerCommand(clean, commandHandler);
+	let dispoFlash = vscode.commands.registerCommand(flash, commandHandler);
+	let dispoShowConfig = vscode.commands.registerCommand(showConfig, showConfigHandler);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sws.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sws!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(dispoBuild);
+	context.subscriptions.push(dispoClean);
+	context.subscriptions.push(dispoFlash);
+	context.subscriptions.push(dispoShowConfig);
 }
 
 // this method is called when your extension is deactivated
