1027e50464fba24d8ad561f784806ddcaf8b64ab
judland <julien.dland@gmail.com>
2020-11-09 16:38:11 +0100

    Ajout de commentaires

--- src/test/mock/mockDispatcher.ts
+++ src/test/mock/mockDispatcher.ts
@@ -3,6 +3,10 @@ import { IDispatcher } from './../../idispatcher';
 import { IEventHandler } from './../../services/IService';
 import * as data from "./mockCommands.json";
 
+/**
+ * This MockDispatcher is used for tests purpose to mock (simulate) a dispatcher
+ * 
+ */
 export class MockDispatcher implements IDispatcher {
     private config: { [index: string]: string };
     // private nil: string = '\x00';
@@ -17,6 +21,14 @@ export class MockDispatcher implements IDispatcher {
     eventHandler(service: string, handler: IEventHandler) { };
     sendEvent(serviceName: string, eventName: string, args: any[]) { };
 
+    /**
+     * 
+     * Return a promise that resolve a typical string for a commandName referenced in the mockCommands.json
+     * 
+     * @param serviceName 
+     * @param commandName Used to search in the json the typical string that we can receive from the command response
+     * @param args 
+     */
     public sendCommand(serviceName: string, commandName: string, args: any[]): Promise<string> {
         let self = this;
         return new Promise(function (resolve, reject) {
