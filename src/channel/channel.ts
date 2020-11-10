5cef811166642eb1a5f3467d6d329703faa66beb
judland <julien.dland@gmail.com>
2020-11-10 11:04:24 +0100

    MINOR add comments for channel

--- src/channel/channel.ts
+++ src/channel/channel.ts
@@ -14,15 +14,28 @@ export class Channel implements IChannel {
         this.localServices = new Map<string, IService>();
         this.remoteServices = new Array<string>();
     }
-
+    
+    /**
+     * Adds a local service to the TCF channel
+     * 
+     * @param service local service to add
+     */
     public setLocalService(service: IService) {
         this.localServices.set(service.getName(), service);
     }
 
+    /**
+     * Returns all local services of the TCF channel
+     */
     public getLocalServices() {
         return this.localServices.values();
     }
 
+    /**
+     * Returns one local service by his name, throws an error if the service is unknown
+     * 
+     * @param name name of the local service to return
+     */
     public getLocalService(name: string) {
         if (this.localServices.has(name)) {
             return this.localServices.get(name) as IService;
@@ -30,10 +43,18 @@ export class Channel implements IChannel {
         throw new Error(`[Channel] Unknown ${name} service`);
     };
 
+    /**
+     * Returns all remotes services of the TCF channel
+     */
     public getRemoteServices() {
         return this.remoteServices;
     }
 
+    /**
+     * Sets all available remote services of the TCF channel
+     * 
+     * @param services remote services
+     */
     public setRemoteServices(services: string[]) {
         this.remoteServices = services;
     }
