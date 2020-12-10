f54e26d00c8ad3f39a4e1bdc8edf461549bac835
judland <julien.dland@gmail.com>
2020-12-10 17:25:47 +0100

    add resolve debug configuration

--- src/activateSWSDebug.ts
+++ src/activateSWSDebug.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { ProviderResult } from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
 import { SwsDebugSession } from './swsDebug';
 
 class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
@@ -11,7 +11,28 @@ class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory 
     }
 }
 
+class SwsConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+    /**
+     * program must be entered to debug it
+     * TODO: add more checks to resolve configuration
+     */
+    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+
+        if (!config.program) {
+            return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
+                return undefined;	// abort launch
+            });
+        }
+
+        return config;
+    }
+}
+
 export function activateSWSDebug(context: vscode.ExtensionContext, factory?: vscode.DebugAdapterDescriptorFactory) {
+    // register a configuration provider for 'sws' debug type
+    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('sws', new SwsConfigurationProvider()));
+
     if (!factory) {
         factory = new InlineDebugAdapterFactory();
     }
