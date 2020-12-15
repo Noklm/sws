39b25b5286e632503bdaf44b5786e0194b2d8615
judland <julien.dland@gmail.com>
2020-12-15 14:19:53 +0100

    Simplification de la configuration de la debug session

--- src/activateSWSDebug.ts
+++ src/activateSWSDebug.ts
@@ -5,6 +5,7 @@ import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
 import { SwsDebugSession } from './swsDebug';
 import { WebsocketDispatcher } from './websocketDispatcher';
+import { InterfaceNameType, InterfaceType } from './services/tool/iprogInterface';
 
 class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
 
@@ -27,15 +28,54 @@ class SwsConfigurationProvider implements vscode.DebugConfigurationProvider {
      * program must be entered to debug it
      * TODO: add more checks to resolve configuration
      */
-    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: SwsDebugConfiguration, token?: CancellationToken): ProviderResult<SwsDebugConfiguration> {
 
         if (!config.program) {
             return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
                 return undefined;	// abort launch
             });
         }
+
         config.tool = `com.atmel.avrdbg.tool.${config.tool}`;
+
+
         return config;
+    }
+    resolveDebugConfigurationWithSubstitutedVariables?(folder: WorkspaceFolder | undefined, debugConfiguration: SwsDebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration>{
+        if (!debugConfiguration.interfaceProperties) {
+            switch (debugConfiguration.interface) {
+                case "JTAG": {
+                    debugConfiguration.interfaceProperties = {
+                        KeepTimersRunning: true,
+                        JtagDbgClock: debugConfiguration.interfaceClock,
+                        JtagProgClock: debugConfiguration.interfaceClock
+                    };
+
+                    break;
+                }
+                case "UPDI": {
+                    debugConfiguration.interfaceProperties = {
+                        KeepTimersRunning: true,
+                        UpdiClock: debugConfiguration.interfaceClock
+                    };
+                    break;
+                }
+                case "SWD": {
+                    debugConfiguration.interfaceProperties = {
+                        KeepTimersRunning: true,
+                        SwdClock: debugConfiguration.interfaceClock
+                    };
+                    break;
+                }
+                default: {
+                    return vscode.window.showInformationMessage("Cannot use this interface").then(_ => {
+                        return undefined;	// abort launch
+                    });
+                }
+            }
+
+        }
+        return debugConfiguration;
     }
 }
 
@@ -51,4 +91,45 @@ export function activateSWSDebug(context: vscode.ExtensionContext, factory?: vsc
         context.subscriptions.push(factory);
     }
 
+}
+
+interface SwsDebugConfiguration extends DebugConfiguration{
+    // ATBACKEND PROPERTIES
+    // atbackendHost: string;
+    // atbackendPort: number;
+
+    // FILE TO DEBUG PROPERTY
+    program: string;
+
+    // TOOL PROPERTIES
+    tool: string;
+    // toolConnection: string;
+    // connectionProperties: IConnectionProperties;
+
+    // DEVICE PROPERTY
+    device: string;
+
+    // COMMUNICATION INTERFACE PROPERTIES
+    interface: InterfaceNameType;
+    interfaceProperties: InterfaceType;
+
+    // OTHER PROPERTIES
+    launchSuspended: boolean;
+    launchAttached: boolean;
+    cacheFlash: boolean;
+
+    eraseRule: number; // enum
+    preserveEeprom: boolean;
+    ramSnippetAddress: number;
+    progFlashFromRam: boolean;
+
+    useGdb: boolean;
+    gdbLocation: string;
+
+    bootSegment: number; // enum
+
+    packPath: string;
+
+    remapSourcePathFrom: string;
+    remapSourcePathTo: string;
 }
