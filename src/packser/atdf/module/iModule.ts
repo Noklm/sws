eb649423ba42a0d81aba525cd2970e7608451b96
judland <julien.dland@gmail.com>
2021-02-10 10:11:22 +0100

    [FIX] IModule

--- src/packser/atdf/module/iModule.ts
+++ src/packser/atdf/module/iModule.ts
@@ -2,6 +2,9 @@ import { IRegisterGroup } from "./iRegisterGroup";
 import { IValueGroup } from "./iValueGroup";
 
 export interface IModule{
+    caption: string
+    name: string
+    id?: string
     "register-group": IRegisterGroup | IRegisterGroup[]
     "value-group"?: IValueGroup | IValueGroup[]
 }
